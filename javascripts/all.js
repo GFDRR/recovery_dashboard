2aee15d4ee6315326f6f7a24a7b2adc1067b6af7
Mila Frerichs <mila.frerichs@gmail.com>
2015-05-08 14:20:08 +0200

    Automated commit at 2015-05-08 12:20:08 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -48597,14 +48597,159 @@ var colorbrewer = {YlGn: {
 (function() {
   window.dashboard.service('layerListModel', [
     '$rootScope', 'styleHelper', function($rootScope, styleHelper) {
-      var damagedBuildingsLayer, hotosmLayer, landslideLayer, mediaLayer, povertyLayer;
+      var damagedBuildingsLayer, hotosmLayer, landslideLayer, mediaLayer, medicalLayer, medicalPolygonLayer, povertyLayer, roadsLayer, schoolLayer, schoolPolygonLayer, trainStationsLayer;
       hotosmLayer = {
         name: 'HOTOSM',
         active: true,
-        opacity: 0.5,
+        index: 0,
         source: {
           type: 'OSM',
           url: 'http://b.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+        }
+      };
+      povertyLayer = {
+        name: 'poverty',
+        active: true,
+        displayed: true,
+        index: 1,
+        source: {
+          type: 'TopoJSON',
+          url: 'data/poverty.json'
+        },
+        style: styleHelper.povertyAvgStyle,
+        selectedStyle: "povertyAvgStyle",
+        styleOptions: [
+          {
+            styleName: "Avg Poor",
+            styleParam: "povertyAvgStyle"
+          }, {
+            styleName: "Absolute # of Poor",
+            styleParam: "povertyAbsStyle"
+          }
+        ],
+        metadata: {
+          name: "Poverty Levels",
+          source: "Worldbank"
+        }
+      };
+      schoolPolygonLayer = {
+        name: 'school-polygon',
+        active: true,
+        displayed: false,
+        visible: false,
+        source: {
+          type: 'GeoJSON',
+          url: 'http://nepal.piensa.co/data/schools_polygon.json'
+        },
+        metadata: {
+          name: "School Polygons",
+          source: "OSM"
+        },
+        style: {
+          fill: {
+            color: "blue"
+          },
+          stroke: {
+            width: 4,
+            color: "blue"
+          }
+        }
+      };
+      trainStationsLayer = {
+        name: 'train_stations',
+        active: true,
+        displayed: false,
+        visible: false,
+        source: {
+          type: 'GeoJSON',
+          url: 'http://nepal.piensa.co/data/train_stations.json'
+        },
+        metadata: {
+          name: "Train stations",
+          source: "OSM"
+        },
+        style: {
+          image: {
+            icon: {
+              src: 'images/icons/rail-12.png'
+            }
+          }
+        }
+      };
+      roadsLayer = {
+        name: 'roads',
+        active: true,
+        displayed: true,
+        source: {
+          type: 'GeoJSON',
+          url: 'http://nepal.piensa.co/data/main_roads.json'
+        },
+        metadata: {
+          name: "Main roads",
+          source: "OSM"
+        }
+      };
+      medicalPolygonLayer = {
+        name: 'medicalpolygon',
+        active: true,
+        displayed: true,
+        source: {
+          type: 'GeoJSON',
+          url: 'http://nepal.piensa.co/data/medical_polygon.json'
+        },
+        metadata: {
+          name: "Medical facilities Polygons",
+          source: "OSM"
+        },
+        style: {
+          fill: {
+            color: "red"
+          },
+          stroke: {
+            width: 4,
+            color: "red"
+          }
+        }
+      };
+      medicalLayer = {
+        name: 'medical',
+        active: true,
+        displayed: true,
+        source: {
+          type: 'GeoJSON',
+          url: 'http://nepal.piensa.co/data/medical_point.json'
+        },
+        style: {
+          image: {
+            icon: {
+              src: 'images/icons/hospital-12.png'
+            }
+          }
+        },
+        metadata: {
+          name: "Medical facilities",
+          source: "OSM"
+        }
+      };
+      schoolLayer = {
+        name: 'school',
+        active: true,
+        displayed: false,
+        visible: false,
+        source: {
+          type: 'GeoJSON',
+          url: 'http://nepal.piensa.co/data/schools_point.json'
+        },
+        metadata: {
+          name: "Schools",
+          source: "OSM"
+        },
+        style: {
+          image: {
+            icon: {
+              src: 'images/icons/school-12.png'
+            }
+          }
         }
       };
       mediaLayer = {
@@ -48649,30 +48794,6 @@ var colorbrewer = {YlGn: {
           source: "Worldbank"
         }
       };
-      povertyLayer = {
-        name: 'poverty',
-        active: true,
-        displayed: true,
-        source: {
-          type: 'GeoJSON',
-          url: 'data/poverty.geojson'
-        },
-        style: styleHelper.povertyAvgStyle,
-        selectedStyle: "povertyAvgStyle",
-        styleOptions: [
-          {
-            styleName: "Avg Poor",
-            styleParam: "povertyAvgStyle"
-          }, {
-            styleName: "Absolute # of Poor",
-            styleParam: "povertyAbsStyle"
-          }
-        ],
-        metadata: {
-          name: "Poverty Levels",
-          source: "Worldbank"
-        }
-      };
       this.layerGroups = [
         {
           name: "Poverty",
@@ -48680,13 +48801,19 @@ var colorbrewer = {YlGn: {
         }, {
           name: "Media",
           layers: [mediaLayer]
+        }, {
+          name: "Infrastructure",
+          layers: [roadsLayer, trainStationsLayer]
+        }, {
+          name: "Public Facilities",
+          layers: [schoolLayer, schoolPolygonLayer, medicalLayer, medicalPolygonLayer]
         }
       ];
       this.list = _.unique(_.flatten([
         _.collect(this.layerGroups, function(group) {
           return group.layers;
-        }), hotosmLayer
-      ]));
+        }).reverse(), hotosmLayer
+      ])).reverse();
       return this;
     }
   ]);
@@ -48737,9 +48864,19 @@ var colorbrewer = {YlGn: {
           position: [0, 0]
         });
         showPopup = function(event, feature, olEvent) {
-          var overlayHidden;
+          var layer, overlayHidden, pixel;
+          pixel = map.getEventPixel(olEvent.originalEvent);
+          layer = map.forEachLayerAtPixel(pixel, function(layer) {
+            return layer;
+          });
           $scope.$apply(function(scope) {
-            return $scope.properties = feature ? feature.getProperties() : {};
+            var layerData;
+            $scope.properties = feature ? feature.getProperties() : {};
+            $scope.name = layer.get('name');
+            layerData = _.find(scope.layerList, {
+              name: $scope.name
+            });
+            return $scope.sourceType = layerData.metadata.source;
           });
           overlayHidden = true;
           if (!feature) {
